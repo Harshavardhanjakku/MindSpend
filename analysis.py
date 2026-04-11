275a402e1b27ac2bcd07a94be4163aa597236bd4
Harshavardhanjakku <2203a51012@sru.edu.in>
2026-04-12 04:03:57 +0530

    Fixed bugs

--- analysis.py
+++ analysis.py
@@ -2,11 +2,23 @@ import json
 import matplotlib.pyplot as plt
 categories={"Food":0,"Travel":0,"Shopping":0,"Bills":0,"Entertainment":0}
 mycolors = [ "blue","pink","green", "yellow",  "purple"]
+lowest=float('inf')
+highest=float('-inf')
+flag=False 
 with open("myexpenses.json") as f :
     data=json.load(f)
-    for expense in data :
-        x=expense['category']
-        categories[x]+=expense['amount']
+    if len(data)==0:
+        lowest=None 
+        highest=None 
+        flag=True
+    else:
+        for expense in data :
+            lowest=min(lowest,expense['amount'])
+            highest=max(highest,expense['amount'])
+            x=expense['category']
+            categories[x]+=expense['amount']
 print(categories)
-plt.pie(categories.values(),labels=categories.keys(),colors=mycolors,autopct='%1.1f%%')
-plt.show()
+print(f"Lowest:{lowest} and Highest :{highest}")
+if not flag:
+    plt.pie(categories.values(),labels=categories.keys(),colors=mycolors,autopct='%1.1f%%')
+    plt.show()
