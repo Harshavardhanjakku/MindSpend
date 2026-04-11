a0ff05f83e90a727be98790a8022107b2610b1f5
Harshavardhanjakku <2203a51012@sru.edu.in>
2026-04-12 01:53:11 +0530

    Added the functions

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import json
-
+class Expense:
+    Total=0
+    def __init__(self,date, category, amount, description):
+        self.date = date 
+        self.category = category
+        self.amount = amount
+        self.description = description
+        Expense.Total+=amount  
+    def convertdict(self):
+        mydic={
+            "date":self.date,
+            "category":self.category,
+            "amount":self.amount,
+            "description":self.description
+        }
+        return mydic 
+    def Totalexpenses():
+        return Expense.Total
+new_expense=Expense("12-04-2026","Travel",2500,"Trip to Vizag")
 expenses = """
 {
     "Harsha": [
@@ -20,9 +38,11 @@ expenses = """
 """
 
 data = json.loads(expenses)
-
+print(type(data))
 tot=0
 for expense in data["Harsha"]:
     print(expense)
     tot+=expense["amount"]
 print("Final Expense:",tot)
+
+
