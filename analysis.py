ca2e45c0829cf6917d921d28be8b55bb932b57b2
Harshavardhanjakku <2203a51012@sru.edu.in>
2026-04-12 03:52:47 +0530

    added matplot piechart

--- analysis.py
+++ analysis.py
@@ -1,10 +1,12 @@
 import json
+import matplotlib.pyplot as plt
 categories={"Food":0,"Travel":0,"Shopping":0,"Bills":0,"Entertainment":0}
-
+mycolors = [ "blue","pink","green", "yellow",  "purple"]
 with open("myexpenses.json") as f :
     data=json.load(f)
     for expense in data :
         x=expense['category']
         categories[x]+=expense['amount']
 print(categories)
-
+plt.pie(categories.values(),labels=categories.keys(),colors=mycolors,autopct='%1.1f%%')
+plt.show()
